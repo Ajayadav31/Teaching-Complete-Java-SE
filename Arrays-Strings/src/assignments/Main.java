d72997bec108ff79582f0207bd2441d518117ead
akashydv311 <akashydv311@gmail.com>
2023-08-30 20:31:17 +0530

    assignments added

---
+++ Arrays-Strings/src/assignments/Main.java
@@ -0,0 +1,16 @@
+package assignments;
+
+/*
+ * 1 - Find the 76 in the given array
+ *     arr[] = {34,45,342,76,78}
+ * 
+ * 2 - Find the largest element in an array
+ *     arr[] = { 34,45, 564, 6785, 43}
+ * 
+ * 3 - Find the smallest element in an array
+ *     arr[] = {45, 323, 12, 34, 6, 78}
+ */
+
+public class Main {
+
+}
