755b2e9b6ccb2a93879feb3880c4e73436e599ac
akashydv311 <akashydv311@gmail.com>
2023-08-30 20:06:58 +0530

    Update assignments

--- Arrays-Strings/src/assigements/ArraysAssigements.java
+++ Arrays-Strings/src/assigements/ArraysAssigements.java
@@ -3,6 +3,9 @@ package assigements;
 /*
  * 1 - Find 10 in the given array
  *      arr[] = {23,34,45,67,78,10,56}
+ * 
+ * 2 - Find largest number in the arry
+ *      arr[] = {23,34,45,67,78,10,56}
  */
 
 public class ArraysAssigements {
