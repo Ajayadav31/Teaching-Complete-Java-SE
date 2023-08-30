91277518eec319586fb2fbe4f952f0e126f56763
akashydv311 <akashydv311@gmail.com>
2023-08-30 19:51:25 +0530

    Arrays arrigement added

---
+++ Arrays-Strings/src/assigements/ArraysAssigements.java
@@ -0,0 +1,12 @@
+package assigements;
+
+/*
+ * 1 - Find 10 in the given array
+ *      arr[] = {23,34,45,67,78,10,56}
+ */
+
+public class ArraysAssigements {
+    public static void main(String[] args) {
+
+    }
+}
